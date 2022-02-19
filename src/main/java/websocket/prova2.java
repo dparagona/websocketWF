00e6029201d07b3f568cd9779be4399c57e4461e
dparagona <paragona.davide@gmail.com>
2022-02-19 19:33:04 +0100

    Update prova2.java

--- src/main/java/websocket/prova2.java
+++ src/main/java/websocket/prova2.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.lang.Thread;
 
 
 @ServerEndpoint(
@@ -35,11 +36,14 @@ public class prova2 {
     private final AreaNameLogic areaNameLogic = new AreaNameLogic(); //serve per ottenere le aree interne ad un riquadro
     private ArrayList<StreetMongo> streetsFromArea = new ArrayList<>(); //array di strade presenti nelle aree richieste, provenienti da mongo
     private ArrayList<Street> streetsWithGeometry = new ArrayList<>();  //array di strade contenenti un array che ne definisce la geometria, provenienti da Neo4J
-    private Map<String, AreaWorker> workers = new HashMap<>();//mappa che contiene i vari workers
+    private Map<String, Thread> workers = new HashMap<>();//mappa che contiene i vari workers
+	private AreaBuffer buffer;
 
     @OnOpen
     public void onOpen(Session session) throws IOException {
         this.session = session;
+		this.buffer = new AreaBuffer(20);
+
         // Registra la sessione in un Set
         provaEndpoints.add(this);
         if (session != null) {
@@ -50,20 +54,8 @@ public class prova2 {
 
     @OnMessage
     public void onMessage(Session session, Message message) throws IOException {
-        System.out.println("flag1 modificata a TRUE");
 
-        if (message instanceof StreetMongo) {
-            System.out.println("Messaggio: " + message);
-            StreetMongo street = (StreetMongo) message;
-            street.print(System.out);
-            session.getBasicRemote().sendText("Oggetto ricevuto con successo!");
-        } else if (message instanceof AreaRequest) {
-            System.out.println("Messaggio: " + message);
-            AreaRequest request = (AreaRequest) message;
-            request.print(System.out);
-            session.getBasicRemote().sendText("Oggetto ricevuto con successo!");
-
-        } else if (message instanceof RequestedSquare) {
+         if (message instanceof RequestedSquare) {
             System.out.println("Messaggio: " + message);
             RequestedSquare square = (RequestedSquare) message;
             for(String key : workers.keySet()){
@@ -79,41 +71,27 @@ public class prova2 {
             int i = 0;
             System.out.println(">>AREE RICEVUTE");
             System.out.println(" ");
-
-			//per ogni area, se la mappa non contiene un worker per n-esima area, ne crea uno, lo abilita e lo esegue, altrimenti lo recupera dalla mappa e lo abilita
+			
+			//bisogna prima pulire la mappa dalle aree che non sono state richieste
+			//per ogni chiave della mappa, se il nuovo insieme di aree richieste non contiene la chiave corrispondente all'i-esimo worker, elimina il worker che non è stato richiesto
+			for(String s: workers.keySet()){
+				if(!areaNames.contains(s)){
+					//elimina il worker perchè non è stata richiesta l'area di sua competenza
+					//workers.get(s).interrupt();
+					workers.remove(s).interrupt();
+				}
+			}
+			//per ogni area, se la mappa non contiene un worker corrispondente, ne istanzia uno, lo aggiunge alla mappa e lo fa partire
             for(String s : areaNames){
 				if(!workers.keySet().contains(s)){
-					i++;
-					System.out.println("Area #" + i + ": " + s+"   Nuova.");
-					//QUI BISOGNA INVOCARE UN THREAD PER OGNI AREA, INOLTRE BISOGNA SALVARE IN UNA MAPPA TUTTI I WORKER LANCIATI
-					ArrayList<String> areas = new ArrayList<>();
-					areas.add(s);
-					AreaWorker worker = new AreaWorker(areas, session);
-					workers.put(s, worker);
-
-					worker.abilitate();
-					worker.start();
-				}else{ //se il worker esiste gia' nella mappa puo' essere o interrotto o non interrotto
-					i++;
-					System.out.println("Area #" + i + ": " + s+"   Woke Up");
-					AreaWorker worker = workers.get(s);
-
-					if(!worker.isInterrupted()){//sta facendo la wait
-						worker.abilitate();
-
-					}else{//e' interrotto
-						System.out.println("Era interrotto.");
-						ArrayList<String> areas = new ArrayList<>();
-						areas.add(s);
-						AreaWorker worker2 = new AreaWorker(areas, session);
-						workers.put(s, worker2);
-						
-						worker2.abilitate();
-						worker2.start();
-					}
+					//alloca tutto il necessario e avvia i processi da avviare
+					Thread produttore = new Thread(new AreaProducer(buffer, s, session));
+					Thread consumatore = new Thread(new AreaConsumer(buffer, s));
+					produttore.start();
+					workers.put(s, consumatore);
+					consumatore.start();
 				}
             }
-			wakeUp();
         }
     }
 
@@ -123,6 +101,7 @@ public class prova2 {
         provaEndpoints.remove(this);
         for (String w : workers.keySet()) {
             workers.remove(w);//pulizia della mappa
+			//bisogna terminare tutti i workerz
         }
     }
 
@@ -131,21 +110,6 @@ public class prova2 {
         throwable.printStackTrace(System.out);
     }
 
-    //si usa per inviare i dati, questo e' costruito per una chat, percio' bisogna capire se deve essere modificato per questa applicazione
-    private static void broadcast(Message message) throws IOException, EncodeException {//
-        provaEndpoints.forEach(endpoint -> {
-            synchronized (endpoint) {
-                try {
-                    endpoint.session.getBasicRemote().sendObject(message);
-                } catch (IOException | EncodeException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-	private synchronized void wakeUp(){
-		notifyAll();
-	}
     private ArrayList<String> getAreaNames(RequestedSquare s) {
         float lon1 = Float.parseFloat(s.getUpperLeftCorner().substring(0, s.getUpperLeftCorner().indexOf(",")));
         float len1 = Float.parseFloat(s.getUpperLeftCorner().substring(s.getUpperLeftCorner().indexOf(",") + 1));
@@ -154,11 +118,247 @@ public class prova2 {
         return areaNameLogic.getAreaNameFromCorners(lon1, len1, lon2, len2);
     }
 }
+//ELEMENTO DEL BUFFER
+class AreaElement{
+	private Session session;
+	private ArrayList<String> areaNames;
+	private HashMap<Long, StreetMongo> streetsFromArea = new HashMap<>(); //array di strade presenti nelle aree richieste, provenienti da mongo
+    private HashMap<Long, Street> streetsWithGeometry = new HashMap<>();  //array di strade contenenti un array che ne definisce la geometria, provenienti da Neo4J
+	private ConfigurationSingleton conf = ConfigurationSingleton.getInstance();    
+	private FeatureCollection featureCollection = new FeatureCollection();
+    private Gson gson = new Gson();
+	private  KafkaConsumer<String, String> consumer;
+
+	public AreaElement(ArrayList<String> areaNames, Session session){
+		this.areaNames = areaNames;
+        this.session = session;
+		//creazione del consumer di kafka (lo faccio qui per non doverlo creare ad ogni invocazione del metodo getStreetTraffic())
+		//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConfig.KAFKA_HOST_LOCAL_NAME+":"+KafkaConfig.KAFKA_PORT);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, conf.getProperty("kafka.hostname") + ":" + conf.getProperty("kafka.port"));//KafkaConfig-->classe che contiene le info del kafka che uso
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "areasConsumerGroup");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        System.out.println("Creo il consumer di kafka");
+		this.consumer = new KafkaConsumer<>(props);
+	}
+
+	//metodi di AreaWorker
+	public void getStreetsTraffic() { // non dovrebbe più essere necessario effettuare la poll in un ciclo while, perchè ce n'è già uno a monte, solo che in questo modo l'output sarà enorme, quindi mi converrà creare un kafka consumer fuori dall'elemento, per poi passarglielo come parametro nel costruttore 
+        
+        this.consumer.subscribe(this.areaNames);//il metodo subscribe vuole solo arraylist, quindi bisogna usare arraylist anche per una sola area
+       // while (true) {
+            System.out.println("Poll eseguito");
+            ConsumerRecords<String, String> streetResults = consumer.poll(Duration.ofMillis(10000));
+            int i = 0;
+            for (ConsumerRecord<String, String> record : streetResults) {
+                i++;
+                String value = record.value();
+                StreetMongo streetMongo = gson.fromJson(value, StreetMongo.class);
+                streetsFromArea.put(Long.valueOf(streetMongo.getLinkid()), streetMongo);
+            }
+           // if (i != 0) {//se i!=0 l'array ha elementi, quindi esco dal while
+            //    System.out.println("Dati prelevati da Kafka, Worker di " + areaNames.get(0)+".");
+             //   i=0;
+			//	break;
+           // }
+       // }
+    }
+	
+	 public void getStreetsFromNeo4J() {
+
+		//apre la connessione 
+		String uri = conf.getProperty("neo4j-core.bolt-uri");
+		String user = conf.getProperty("neo4j-core.user");
+		String password = conf.getProperty("neo4j-core.password");
+		Neo4jDAOImpl database = new Neo4jDAOImpl(uri, user, password);
+		database.openConnection();
+
+        System.out.println("Recuperando i dati da Neo4j...");
+		try{
+			ArrayList<Street> streets = database.getStreetsFromLinkIds(streetsFromArea.keySet());
+			for (Street s: streets){
+				streetsWithGeometry.put(s.getLinkId(), s);
+			}
+			System.out.println("Strade Recuperate");
+		}catch(org.neo4j.driver.exceptions.NoSuchRecordException e){
+			System.out.println("Strade non trovate, c'e' stato un problema con neo4j");
+		}
+    }
+
+	public void convertToFeatures() {
+        System.out.println("Conversione dati in formato geojson...");
+        for(Long key : streetsWithGeometry.keySet()){
+            Street s = streetsWithGeometry.get(key);
+            Properties props = new Properties();
+            Geometry geoms = new Geometry();
+            ArrayList<Coordinate> coord = s.getGeometry();
+            for (Coordinate c : coord) {
+                geoms.addGeometry(c.getLongitude(), c.getLatitude());
+            }
+            props.put("name", s.getName());
+            if (s.getFfs() > 20)
+                props.put("color", "#1199dd");
+            else {
+                props.put("color", "#d21f1b");
+            }
+            Feature feature = new Feature(geoms, props);
+            if (!feature.isEmpty())
+                featureCollection.addFeature(feature);
+        }
+    }
+	
+	public synchronized void send() throws IOException {
+        if (!streetsWithGeometry.isEmpty()) {
+            AreaResponse response = new AreaResponse(areaNames.get(0), featureCollection);
+            //String toClient = gson.toJson(featureCollection);
+            String toClient = gson.toJson(response);
+            //System.out.println(toClient);
+            this.session.getBasicRemote().sendText(toClient);
+
+            System.out.println("JSON inviato al client");
+        }
+    }
+
+	//utilities
+	public String getAreaName(){
+		return this.areaNames.get(0);
+	}
+	public Session getSession(){
+		return this.session;
+	}
+}
+//BUFFER
+class AreaBuffer{
+	private LinkedList<AreaElement> coda;
+	private int sizeMax;
+
+	public AreaBuffer(int sizeMax){
+		coda = new LinkedList();
+		this.sizeMax = sizeMax;
+	}
+	public boolean isEmpty(){
+		return coda.isEmpty();
+	}
+	public boolean isFull(){
+		return coda.size() >= this.sizeMax;
+	}
+	public synchronized void aggiungiAreaElement(AreaElement element){
+		while(isFull()){
+			try{
+				wait();
+			}catch(InterruptedException exc){
+				System.err.println("InterruptedException");
+			}
+		}
+		coda.addFirst(element);
+		notifyAll();
+	}
+	public synchronized AreaElement prelevaAreaElement(String areaName){
+		while(isEmpty()){
+			try{
+				wait();
+			}catch(InterruptedException exc){
+				System.err.println("InterruptedException");
+			}
+		}
+
+		for(AreaElement e: coda){
+			if(e.getAreaName() == areaName){
+				int position = coda.indexOf(e);
+				AreaElement element = coda.get(position);
+				coda.remove(position);
+			}
+		}
+		//AreaElement element = coda.removeFirst();
+		notifyAll();
+		return element;
+	}
+}
+//PRODUTTORE
+class AreaProducer implements Runnable{
+	private AreaBuffer buffer;
+	private String areaName;
+	private Session session;
+	private AreaElement element;
+
+	public AreaProducer(AreaBuffer buffer, String areaName, Session session){
+		this.buffer = buffer;
+		this.areaName = areaName;
+		this.session = session;
+
+		ArrayList<String> areas = new ArrayList();
+		areas.add(this.areaName);
+
+		this.element = new AreaElement(areas, this.session);
+	}
+
+	@Override
+	public void run(){
+		
+		//while(true){
+		//	if(buffer.contains(this.element)){
+			//	break; //devo capire se dopo il break devo interrompere questo threadz
+			//}
+			try{
+				//inserisce l'istanza di elemento nel buffer
+				buffer.aggiungiAreaElement(element);//non serve un while perchè questo thread viene messo in attesa se il buffer è pieno, per cui appena il buffer si svuota, questo aggiunge l'elemento al buffer
+
+			}catch(InterruptedException exc){
+				System.err.println("InterruptedException");
+			}
+		//}
+	}
+}
+//CONSUMATORE
+class AreaConsumer implements Runnable{
+	private AreaBuffer buffer;
+	private String areaName;
+	private AreaElement element;
+
+	public AreaConsumer(AreaBuffer buffer, String areaName){
+		this.buffer = buffer;
+		this.areaName = areaName;
+	}
+
+
+	@Override
+	public void run(){//per non stressare troppo i database e il server, si può chiamare questo metodo con un ritardo
+		while(true){
+			if(element == null){//se non c'è un'istanza di element bisogna mettersi in coda al buffer
+				try{
+					AreaElement element = buffer.prelevaAreaElement(areaName);
+				}catch(InterruptedException exc){
+					System.err.println("InterruptedException");
+				}
+			}
+			//chiama i metodi sull'istanza di element(nel caso questo worker abbia già un'istanza di element, non ci sarà bisogno di mettersi in coda al buffer
+				//preleva i dati da kafka usando l'area contenuta in areaNames
+                this.element.getStreetsTraffic();
+                //preleva i dati da Neo4J tramite LongID
+                this.element.getStreetsFromNeo4J();
+                //converte i dati in formato GeoJson
+                this.element.convertToFeatures();
+                //invio i dati
+                if (this.getSession().isOpen()) {
+                    try {
+                        this.element.send();
+                    } catch (IOException e) {
+                        System.out.println("Qualcosa e' andato storto durante l'invio del GeoJson.");
+                        e.printStackTrace();
+                    }
+                }else{
+					System.out.println("Sessione chiusa.");
+				}
+		}
+	}
+
+	public AreaElement getElement(){return this.element;}
+}
 
 class AreaWorker extends Thread {
     private ArrayList<String> areaNames;
     private Session session;
-    private final AreaNameLogic areaNameLogic = new AreaNameLogic(); //serve per ottenere le aree interne ad un riquadro
     private HashMap<Long, StreetMongo> streetsFromArea = new HashMap<>(); //array di strade presenti nelle aree richieste, provenienti da mongo
     private HashMap<Long, Street> streetsWithGeometry = new HashMap<>();  //array di strade contenenti un array che ne definisce la geometria, provenienti da Neo4J
     private Boolean flag1 = true;
@@ -170,7 +370,6 @@ class AreaWorker extends Thread {
     private Neo4jDAOImpl database = new Neo4jDAOImpl(uri, user, password);
     private FeatureCollection featureCollection = new FeatureCollection();
     private Gson gson = new Gson();
-    int i;
 
     public AreaWorker(ArrayList<String> areaNames, Session session) {
         this.areaNames = areaNames;
@@ -221,13 +420,13 @@ class AreaWorker extends Thread {
     private void getStreetsTraffic() {
         Properties props = new Properties();
 
-//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConfig.KAFKA_HOST_LOCAL_NAME+":"+KafkaConfig.KAFKA_PORT);//KafkaConfig-->classe che contiene le info del kafka che uso
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConfig.KAFKA_HOST_LOCAL_NAME+":"+KafkaConfig.KAFKA_PORT);
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, conf.getProperty("kafka.hostname") + ":" + conf.getProperty("kafka.port"));//KafkaConfig-->classe che contiene le info del kafka che uso
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "areasConsumerGroup");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        System.out.println("Creo il consumer");
+        System.out.println("Creo il consumer di kafka");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);//#1: KEY, #2: VALUE
         consumer.subscribe(areaNames);//purtroppo il metodo subscribe vuole solo arraylist, quindi bisogna usare arraylist anche per una sola area
 
@@ -235,7 +434,7 @@ class AreaWorker extends Thread {
         while (true) {//usa una variabile booleana che viene settata a true ogni volta che un nuovo messaggio viene ricevuto
             System.out.println("Poll eseguito");
             ConsumerRecords<String, String> streetResults = consumer.poll(Duration.ofMillis(10000));
-            i = 0;
+            int i = 0;
             for (ConsumerRecord<String, String> record : streetResults) {
                 i++;
                 String value = record.value();
@@ -284,7 +483,6 @@ class AreaWorker extends Thread {
         System.out.println("Conversione dati in formato geojson...");
         for(Long key : streetsWithGeometry.keySet()){
             Street s = streetsWithGeometry.get(key);
-//        for (Street s : streetsWithGeometry) {
             Properties props = new Properties();
             Geometry geoms = new Geometry();
             ArrayList<Coordinate> coord = s.getGeometry();
