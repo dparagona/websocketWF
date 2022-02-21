9df4c06cdc54bf12afe38aa65e9c309dbf7d3fd9
dparagona <paragona.davide@gmail.com>
2022-02-21 17:26:10 +0100

    Update prova2.java

--- src/main/java/websocket/prova2.java
+++ src/main/java/websocket/prova2.java
@@ -71,9 +71,9 @@ public class prova2 {
 			
 			//bisogna prima pulire la mappa dalle aree che non sono state richieste
 			//per ogni chiave della mappa, se il nuovo insieme di aree richieste non contiene la chiave corrispondente all'i.esimo worker, elimina il worker che non e' stato richiesto
-			Iterator it = workers.keySet().iterator();
-			for(Map.Entry<String, Thread> entry : workers.entrySet()){
-				String s = entry.getKey();
+			Iterator it = workers.entryySet().iterator();
+			while(it.hasNext()){
+				String s = it.next().getKey();
 				System.out.println(s);
 				if(!areaNames.contains(s)){
 					//elimina il worker perche' non e' stata richiesta l'area di sua competenza
