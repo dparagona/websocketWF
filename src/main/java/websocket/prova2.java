a837b4415b0aa88b9307c51ea6e4fe6e2f3faca6
dparagona <paragona.davide@gmail.com>
2022-02-22 19:04:37 +0100

    Update prova2.java

--- src/main/java/websocket/prova2.java
+++ src/main/java/websocket/prova2.java
@@ -386,7 +386,7 @@ class AreaConsumer implements Runnable{
 						try {
 							this.element.send();
 							try {
-							Thread.sleep(3000);//180 000 millisecondi = 3 minuti
+							Thread.sleep(10000);//180 000 millisecondi = 3 minuti
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
